d8aae686b5ecb2a478e80b2ba8e87b86a51c2528
Pranav P Ninawe <pn4944@gmail.com>
2024-08-22 18:16:05 +0530

    Adding Keypress Function

--- game.js
+++ game.js
@@ -5,22 +5,40 @@ var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+var level =0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequnce();
+        started =true;
+    }
+    
+
+});
+
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     //console.log(userClickedPattern);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
  
- });
-
+});
 
 function nextSequnce(){
+
+    level++;
+    $("#level-title").text("Level "+level);
+    
     var randomNumber = Math.floor((Math.random()*4));
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-
 }
 
 function playSound(name){
@@ -28,5 +46,13 @@ function playSound(name){
     audio.play();
 }
 
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100);
+}
 
+function checkAnswer(currentLevel){
 
+}
